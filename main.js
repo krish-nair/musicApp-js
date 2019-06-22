e6d375738e4c9d1860e75ca9a50243252456ad68
krish-nair <krish.nair101@gmail.com>
2019-06-23 11:58:11 +1200

    Added a function to create the balls

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "#603d94",
+    "#c74b4b",
+    "#b39a2e",
+    "#34b86b",
+    "#2b2727",
+    "#9c5487"
+  ];
 
   //Add the sound
   pads.forEach((pad, num) => {
     pad.addEventListener("click", function() {
+      sounds[num].currentTime = 0;
       sounds[num].play();
+
+      createBalls(num);
     });
   });
+
+  //Create a function that makes colorful balls
+  const createBalls = num => {
+    const ball = document.createElement("div");
+    visual.appendChild(ball);
+    ball.style.backgroundColor = colors[num];
+    ball.style.animation = "jump 2s ease";
+    ball.addEventListener("animationend", function() {
+      visual.removeChild(this);
+    });
+  };
 });
